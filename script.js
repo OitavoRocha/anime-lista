9ce79c12b343a4e9ce5a2301fc4870b1855d58ae
Otávio Rocha <94090679+OitavoRocha@users.noreply.github.com>
2021-11-16 15:05:55 -0300

    Adicionei arquivos

---
+++ script.js
@@ -0,0 +1,112 @@
+var parte = document.getElementById("parte");
+var imagem = document.getElementById("imagemInfo");
+
+parte.style.display = "none";
+
+function adicionarA1() {
+    parte.style.display = "block";
+    imageInfo.src = "https://cdn.myanimelist.net/images/anime/3/67177.jpg"
+    info.innerHTML = "Shigatsu wa Kimi no Uso";
+    episodios.innerHTML = "Episódios: 22";
+    lancamento.innerHTML = "Lançamento: Outubro/2014";
+    sinopse.innerHTML = "Arima Kousei é o tipo de pessoa que você pode chamar de gênio prodígio, desde"+
+    "pequeno sendo um dos melhores pianistas infantis do país. Porém, desde que sua" +
+    "mãe, e também instrutora, morreu, Kousei não consegue mais ouvir os sons de" +
+    "seu piano, e se perdeu completamente na carreira. Até o momento em que ele" +
+    "conhece Kaori, uma jovem violinista cheia de energia que tenta tirar Kousei de seu atual estado.";
+}
+
+function adicionarA2() {
+  parte.style.display = "block";
+  imageInfo.src = "https://cdn.myanimelist.net/images/anime/10/78745.jpg";
+  info.innerHTML = "Boku no Hero Academia";
+  episodios.innerHTML = "Episódios: 13";
+  lancamento.innerHTML = "Lançamento: Abril/2016";
+  sinopse.innerHTML = "Em um mundo onde ter super poderes, conhecidos como Individualidades, é algo comum e corriqueiro, nascer sem uma destas é considerado estranho, fora do padrão." +
+  "Desse modo, é muito fácil virar alvo de piadas e preconceitos por partes da sociedade em geral, principalmente quando seu sonho é se tornar o super herói n° 1."+
+  "E é assim que vive Izuku, um garoto sem poderes e com um sonho grandioso, sem muita perspectiva de um futuro em que o tal se realize, até que tem um encontro com o seu herói favorito.";
+}
+
+function adicionarA3() {
+  parte.style.display = "block";
+  imageInfo.src = "https://cdn.myanimelist.net/images/anime/8/59955.jpg";
+  info.innerHTML = "Haikyuu!!";
+  episodios.innerHTML = "Episódios: 25";
+  lancamento.innerHTML = "Lançamento: Abril/2014";
+  sinopse.innerHTML = "Nesse anime, acompanhamos a trajetórai do time de vôlei masculino da escola de ensino médio Karasuno, indo em busca do seu objetivo," +
+  " conseguir fazer com que o time volte mais uma vez ao campeonato nacional, depois de um hiato de anos.";
+}
+
+function adicionarA4() {
+  parte.style.display = "block";
+  imageInfo.src = "https://cdn.myanimelist.net/images/anime/13/50521.jpg";
+  info.innerHTML = "Hyouka";
+  episodios.innerHTML = "Episódios: 22";
+  lancamento.innerHTML = "Lançamento: Abril/2012";
+  sinopse.innerHTML = "Oreki pode ser considerado um típico aluno do ensino médio, se não fosse pela" +
+  "sua filosofia de vida que o distingue dos outros: conservar o máximo de energia" +
+  "possível. Ou seja, Oreki sempre se limita atos que não perturbem seu estado" +
+  "físico. Porém, tudo isso muda quando ele entra para o Clube de Literatura" +
+  "Clássica, onde conhece Chitanda, uma garota cheia de energia e sempre curiosa, e" +
+  "acaba envolvido na investigação de um mistério de mais de 45 anos que recai sobre o clube";
+}
+
+function adicionarA5() {
+  parte.style.display = "block";
+  imageInfo.src = "https://cdn.myanimelist.net/images/anime/1466/99748.jpg";
+  info.innerHTML = "Kimetsu no Yaiba";
+  episodios.innerHTML = "Episódios: 26";
+  lancamento.innerHTML = "Lançamento: Abril/2019";
+  sinopse.innerHTML = "Onis são demônios da cultura japonesa que se alimentam de humanos, insaciáveis, muitos mais fortes fisicamente que humanos e, pior, monstros reais." +
+  "A primeira e única frente de ação contra essas medonhas criaturas é a organização dos Caçadores de Onis, e é entrando nessa organização que nosso personagem principal, " +
+  "Kamado Tanjirou, busca a vingança pelo assasinato de sua família nas mãos de um oni.";
+}
+
+function adicionarA6() {
+  parte.style.display = "block";
+  imageInfo.src = "https://cdn.myanimelist.net/images/anime/1476/104943.jpg";
+  info.innerHTML = "Black Clover";
+  episodios.innerHTML = "Episódios: 170";
+  lancamento.innerHTML = "Lançamento: Outubro/2017";
+  sinopse.innerHTML = "Num mundo onde magia é algo normal e corriqueiro, onde todos os humanos nascem com um nível de mana dentro de si, nós seguimos a história de dois" +
+  "garotos que foram abandonados no mesmo dia em frente a uma igreja. Yuno, com " +
+  "um poder mágico incrível, chegando a superar até mesmo a realeza, e Asta, um " +
+  "humano que nasceu com um defeito, sem magia nenhuma no corpo, perseguindo seu sonho de se tornor o rei mago, o mais poderoso de todo o reino";
+}
+
+function adicionarA7() {
+  parte.style.display = "block";
+  imageInfo.src = "https://cdn.myanimelist.net/images/anime/11/33657.jpg";
+  info.innerHTML = "Hunter x Hunter";
+  episodios.innerHTML = "Episódios: 148";
+  lancamento.innerHTML = "Lançamento: Outubro/2011";
+  sinopse.innerHTML = "A profissão de Hunter é no geral bem perigosa, sendo desde caçadores de recompensas até exploradores. Gon, um menino à primeira vista comum, busca" +
+  " tornar-se um Hunter para poder encontrar seu pai. Junto com ele, vamos descobrir um mundo muito rico, com diferentes monstros e aventuras, enquanto acompnhamos o nosso protagonista.";
+}
+
+function adicionarA8() {
+  parte.style.display = "block";
+  imageInfo.src = "https://cdn.myanimelist.net/images/anime/9/77484l.jpg";
+  info.innerHTML = "Kimi no Na wa.";
+  episodios.innerHTML = "Episódios: 1 - Filme";
+  lancamento.innerHTML = "Lançamento: Agosto/2016";
+  sinopse.innerHTML = " Mitsuha Miyamizu, uma menina do ensino médio, anseia por viver a vida de um garoto na movimentada cidade de Tóquio - um sonho que contrasta fortemente com sua vida atual no campo. Enquanto isso, na cidade, Taki Tachibana vive uma vida agitada como estudante do ensino médio, enquanto faz malabarismos com seu emprego de meio período e espera um futuro na arquitetura. Um dia, Mitsuha acorda em uma sala que não é dela e de repente se vê vivendo a vida dos sonhos em Tóquio - mas no corpo de Taki! Em outros lugares, Taki se vê vivendo a vida de Mitsuha no campo humilde. Em busca de uma resposta para esse fenômeno estranho, eles começam a procurar um ao outro. Kimi no Na wa.";
+}
+
+function adicionarA9() {
+  parte.style.display = "block";
+  imageInfo.src = "https://cdn.myanimelist.net/images/anime/1046/107701.jpg";
+  info.innerHTML = "Jujutsu Kaisen";
+  episodios.innerHTML = "Episódios: 24";
+  lancamento.innerHTML = "Lançamento: Outubro/2020";
+  sinopse.innerHTML = "Seu enredo foca em uma mistura de ação com sobrenatural e nos traz Yuji, um jovem atleta até então vivendo uma vida comum. No entanto, ao participar do Clube de Estudo de Fenômenos Ocultos, acaba por entrar em contato com um espírito real que surge em sua escola.";
+}
+
+function adicionarA0() {
+  parte.style.display = "block";
+  imageInfo.src = "https://cdn.myanimelist.net/images/anime/3/80136l.jpg";
+  info.innerHTML = "Koe no Katachi";
+  episodios.innerHTML = "Episódios: 1 - Filme";
+  lancamento.innerHTML = "Lançamento: Setembro/2016";
+  sinopse.innerHTML = "Uma estudante com problemas de audição sofre com o bullying dos colegas e decide mudar de escola. Anos mais tarde, um dos rapazes que a importunavam resolve se redimir.";
+}
---
+++ script2.js
@@ -0,0 +1,44 @@
+function adicionarT1(){
+    var video1 = document.getElementById("v1");
+    video1.style.display = "block";
+    var video2 = document.getElementById("v2");
+    video2.style.display = "none";
+    var video3 = document.getElementById("v3");
+    video3.style.display = "none";
+    var video4 = document.getElementById("v4");
+    video4.style.display = "none";
+    var video5 = document.getElementById("v5");
+    video5.style.display = "none";
+}
+
+function adicionarT2(){
+    var video2 = document.getElementById("v2");
+    video2.style.display = "block";
+    var video3 = document.getElementById("v3");
+    video3.style.display = "none";
+    var video4 = document.getElementById("v4");
+    video4.style.display = "none";
+    var video5 = document.getElementById("v5");
+    video5.style.display = "none";
+}
+
+function adicionarT3(){
+    var video3 = document.getElementById("v3");
+    video3.style.display = "block";
+    var video4 = document.getElementById("v4");
+    video4.style.display = "none";
+    var video5 = document.getElementById("v5");
+    video5.style.display = "none";
+}
+
+function adicionarT4(){    
+    var video4 = document.getElementById("v4");
+    video4.style.display = "block";
+    var video5 = document.getElementById("v5");
+    video5.style.display = "none";
+}
+
+function adicionarT5(){
+    var video5 = document.getElementById("v5");
+    video5.style.display = "block";
+}
